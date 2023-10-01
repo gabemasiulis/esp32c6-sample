dc07a1b33fc42ff3e0c5f99d28b7b2f0291549c3
Gabe Masiulis <gabe@codeofthenorth.com>
2023-09-30 20:52:02 -0500

    working LED code and Wifi Connection code. Still progress needed on POSTing a body on an http client request

--- build.rs
+++ build.rs
@@ -1,6 +1,12 @@
 // Necessary because of this issue: https://github.com/rust-lang/cargo/issues/9641
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> anyhow::Result<()> {
+
+    // Check if the `cfg.toml` file exists and has been filled out.
+    if !std::path::Path::new("cfg.toml").exists() {
+        anyhow::bail!("You need to create a `cfg.toml` file with your Wi-Fi credentials! Use `cfg.toml.example` as a template.");
+    }
+
+    // Necessary because of this issue: https://github.com/rust-lang/cargo/issues/9641
     embuild::build::CfgArgs::output_propagated("ESP_IDF")?;
-    embuild::build::LinkArgs::output_propagated("ESP_IDF")?;
-    Ok(())
+    embuild::build::LinkArgs::output_propagated("ESP_IDF")
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,46 @@
 use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
 use log::*;
 use esp_idf_hal::delay::FreeRtos;
+use esp_idf_svc::eventloop::EspSystemEventLoop;
 use esp_idf_hal::gpio::*;
 use esp_idf_hal::peripherals::Peripherals;
+
+use esp_idf_svc::http::client::{Configuration, EspHttpConnection};
+use embedded_svc::utils::io;
+
+use embedded_svc::http::client::Client as HttpClient;
+use embedded_svc::http::Method;
+
+// use esp_idf_svc::nvs::EspDefaultNvsPartition;
+mod rgb_led;
+
+use esp_idf_sys as _;
+
+mod wifi;
+mod config;
+
+fn create_client() -> anyhow::Result<HttpClient<EspHttpConnection>> {
+    let config = Configuration {
+        use_global_ca_store: true,
+        crt_bundle_attach: Some(esp_idf_sys::esp_crt_bundle_attach),
+        ..Default::default()
+    };
+
+    Ok(HttpClient::wrap(EspHttpConnection::new(&config)?))
+}
+
+fn post_test(client: &mut HttpClient<EspHttpConnection>, url: &str) {
+    let headers = [("content-type", "application/json")];
+    let mut req = client.request(Method::Post, &url, &headers).unwrap();
+    println!("request initiated");
+    req.write(&"Hello World!".as_bytes());
+    println!("request appended maybe?");
+    let mut res = req.submit().unwrap();
+    println!("Request submitted");
+    let status = res.status();
+    println!("Response status: {status}");
+
+}
 
 fn main() {
     // It is necessary to call this function once. Otherwise some patches to the runtime
@@ -12,42 +50,35 @@ fn main() {
     // esp_idf_svc::log::EspLogger::initialize_default();
 
     // info!("Hello, world!");
-    let peripherals = Peripherals::take().unwrap();
-    let mut led = PinDriver::output(peripherals.pins.gpio4).unwrap();
-    let mut onboard_led = PinDriver::output(peripherals.pins.gpio8).unwrap();
-    onboard_led.set_high().unwrap();
     println!("Hello world!");
-    loop {
-        led.set_high().unwrap(); // .set_high();
-        // println!("Set High");
-        // we are sleeping here to make sure the watchdog isn't triggered
-        FreeRtos::delay_ms(2000);
+    let peripherals = match Peripherals::take() {
+        Some(x) => x,
+        None => {
+            return;
+        } 
+    };
+    // if let OK(sysloop) = EspSystemEventLoop::take() {
 
-        led.set_low().unwrap();
-        // println!("Set Low");
-        FreeRtos::delay_ms(2000);
-    }
+    // }
+    let sysloop = match EspSystemEventLoop::take(){
+        Ok(sysloop) => sysloop,
+        Err(x) => {return}
+    };
+
+
+    let mut led = rgb_led::WS2812RMT::new(peripherals.pins.gpio8, peripherals.rmt.channel0).unwrap();
+    led.set_pixel(rgb_led::RGB8::new(5, 5, 50));
+
+    let app_config = config::CONFIG;
+    println!("SSID: {}, PSK: {}", app_config.wifi_ssid, app_config.wifi_psk);
+
+    let _wifi = wifi::wifi(
+        app_config.wifi_ssid,
+        app_config.wifi_psk,
+        peripherals.modem,
+        sysloop,
+    ).expect("failed to connect");
+
+    let mut client = create_client().unwrap();
+    post_test(&mut client, "https://rustembedded.requestcatcher.com/");
 }
-// use std::thread;
-// use std::time::Duration;
-// use esp_idf_sys as _;
-// use embedded_hal::digital::blocking::OutputPin;
-// use esp_idf_hal::peripherals::Peripherals;
-
-// fn main() {
-//   esp_idf_sys::link_patches();
-
-//   let peripherals = Peripherals::take().unwrap();
-//   let mut led = peripherals.pins.gpio5.into_output().unwrap();
-//   let n = 1;
-
-//   while n == 1 {
-//     led.set_high().unwrap();
-//     thread::sleep(Duration::from_millis(1000));
-
-//     led.set_low().unwrap();
-//     thread::sleep(Duration::from_millis(1000));
-
-//     println!("blink");
-//   }
-// }
