3dcf8bcf761b9d35675ea81d31bc5835ac14e40a
Gabe Masiulis <gabe@codeofthenorth.com>
2023-10-01 20:28:10 -0500

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,8 @@
 use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
-use log::*;
-use esp_idf_hal::delay::FreeRtos;
 use esp_idf_svc::eventloop::EspSystemEventLoop;
-use esp_idf_hal::gpio::*;
 use esp_idf_hal::peripherals::Peripherals;
 
 use esp_idf_svc::http::client::{Configuration, EspHttpConnection};
-use embedded_svc::utils::io;
 
 use embedded_svc::http::client::Client as HttpClient;
 use embedded_svc::http::Method;
@@ -33,9 +29,9 @@ fn post_test(client: &mut HttpClient<EspHttpConnection>, url: &str) {
     let headers = [("content-type", "application/json")];
     let mut req = client.request(Method::Post, &url, &headers).unwrap();
     println!("request initiated");
-    req.write(&"Hello World!".as_bytes());
+    req.write(&"Hello World!".as_bytes()).expect("Send a hello world post body");
     println!("request appended maybe?");
-    let mut res = req.submit().unwrap();
+    let res = req.submit().unwrap();
     println!("Request submitted");
     let status = res.status();
     println!("Response status: {status}");
@@ -57,17 +53,14 @@ fn main() {
             return;
         } 
     };
-    // if let OK(sysloop) = EspSystemEventLoop::take() {
-
-    // }
     let sysloop = match EspSystemEventLoop::take(){
         Ok(sysloop) => sysloop,
-        Err(x) => {return}
+        Err(_) => {return}
     };
 
 
     let mut led = rgb_led::WS2812RMT::new(peripherals.pins.gpio8, peripherals.rmt.channel0).unwrap();
-    led.set_pixel(rgb_led::RGB8::new(5, 50, 5));
+    led.set_pixel(rgb_led::RGB8::new(5, 50, 5)).expect("set the led to green");
 
     let app_config = config::CONFIG;
     println!("SSID: {}, PSK: {}", app_config.wifi_ssid, app_config.wifi_psk);
